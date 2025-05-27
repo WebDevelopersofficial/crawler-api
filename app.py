ef5e2fe7ac39ea96240d06ad02c3da9555ffb29a
WebDevelopers <webdevelopersofficial@gmail.com>
2025-05-28 01:44:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from main import app
 
+# This file needs to properly expose the ASGI application for Gunicorn to use
+# We need to create a proper ASGI application instance
+
+# Import the ASGI middleware for compatibility with Gunicorn
+from uvicorn.middleware.asgi_wsgi import ASGIMiddleware
+
+# Create a WSGI-compatible version of the ASGI application
+application = ASGIMiddleware(app)
+
 # This file is just a wrapper to make Render.com happy
 # It expects app.py with 'app' variable by default 
